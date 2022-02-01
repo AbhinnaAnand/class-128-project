c39a341e7f96cdca7f07ca15b18ced5ec73df822
AbhinnaAnand <83014928+AbhinnaAnand@users.noreply.github.com>
2022-02-01 17:51:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+leftwristx = 0;
+leftwristy = 0;
+rightwristx = 0;
+rightwristy = 0;
+
+song1="";
+song2="";
+function preload(){
+    song1=loadSound("believer.mp3");
+    song2=loadSound("music.mp3");
+}
+
+function setup(){
+    canvas=createCanvas(600,500);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+    posenet = ml5.poseNet(video, modelloaded);
+    posenet.on('pose', gotposes);
+}
+
+function modelloaded() {
+    console.log("posenet is initialized");
+}
+
+function gotposes(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftwristx = results[0].pose.leftWrist.x;
+        leftwristy = results[0].pose.leftWrist.y;
+        console.log("left wrist x = " + leftwristx + " left wrist y = " + leftwristy);
+        rightwristx = results[0].pose.rightWrist.x;
+        rightwristy = results[0].pose.rightWrist.y;
+        console.log("right wrist x = " + rightwristx + " right wrist y = " + rightwristy);
+    }
+}
+
+function draw(){
+    image(video,0,0,600,500);
+}
+
+function play(){
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
